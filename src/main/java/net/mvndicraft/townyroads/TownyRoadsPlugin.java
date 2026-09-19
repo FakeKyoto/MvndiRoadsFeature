197fe183723884e0bb409ae6a758d4cb2441b69a
Z User <z@container>
2026-09-19 10:57:33 +0000

    Display road chunks in /towny map hud
    Road chunks now show with distinct symbols on the Towny map HUD:
    - '=' for validated roads (gold hover text)
    - '~' for un-validated roads (dark red hover text)
    Previously road chunks were invisible (displayed as wilderness).
    
    Hooks into WildernessMapEvent, which fires for each wilderness chunk
    on both /towny map and /towny map hud.

--- src/main/java/net/mvndicraft/townyroads/TownyRoadsPlugin.java
+++ src/main/java/net/mvndicraft/townyroads/TownyRoadsPlugin.java
@@ -12,6 +12,7 @@ import net.mvndicraft.townyroads.data.RoadStorage;
 import net.mvndicraft.townyroads.data.RoadStorageFile;
 import net.mvndicraft.townyroads.listeners.TownyRoadPlayersListener;
 import net.mvndicraft.townyroads.listeners.TownyRoadTownAndNationListener;
+import net.mvndicraft.townyroads.listeners.TownyMapListener;
 import net.mvndicraft.townyroads.settings.Settings;
 import net.mvndicraft.townyroads.settings.TownyRoadsSettings;
 import net.mvndicraft.townyroads.util.Translations;
@@ -48,6 +49,7 @@ public class TownyRoadsPlugin extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new TownyRoadPlayersListener(), this);
         getServer().getPluginManager().registerEvents(new TownyRoadTownAndNationListener(), this);
+        getServer().getPluginManager().registerEvents(new TownyMapListener(), this);
 
         PaperCommandManager manager = new PaperCommandManager(this);
         manager.registerCommand(new TownyRoadsCommand());
