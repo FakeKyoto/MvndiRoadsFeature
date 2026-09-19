faa6d93b40f027b92f1f2dd06fa34e4101bd4ce4
Z User <z@container>
2026-09-19 12:25:36 +0000

    Fix particle effect: repeat for 2s on main thread + 3 particles per point
    The previous version spawned 1 particle per border point instantly, which
    was nearly invisible. Now uses BukkitRunnable to repeat the effect every
    5 ticks for 40 ticks (2 seconds), with 3 particles per point and slight
    random offset for better visibility.

--- src/main/java/net/mvndicraft/townyroads/util/RoadClaimEffect.java
+++ src/main/java/net/mvndicraft/townyroads/util/RoadClaimEffect.java
@@ -5,12 +5,13 @@ import com.palmergames.bukkit.towny.object.WorldCoord;
 import com.palmergames.bukkit.towny.utils.BorderUtil;
 import com.palmergames.bukkit.util.BukkitParticle;
 import net.mvndicraft.townyroads.Road;
-import net.mvndicraft.townyroads.settings.TownyRoadsSettings;
+import net.mvndicraft.townyroads.TownyRoadsPlugin;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.function.Consumer;
 
@@ -30,11 +31,22 @@ public final class RoadClaimEffect {
             ? Color.fromRGB(0xFF, 0xA5, 0x00)
             : Color.fromRGB(0x8B, 0x45, 0x13);
 
-        Consumer<Location> particleSpawner = loc -> {
-            world.spawnParticle(particle, loc, 1, 0, 0, 0, 0,
+        Consumer<Location> spawner = loc -> {
+            world.spawnParticle(particle, loc, 3, 0.15, 0, 0.15, 0,
                 new Particle.DustOptions(color, 2.0f));
         };
 
-        border.runBorderedOnSurface(1, 1, particleSpawner);
+        new BukkitRunnable() {
+            int ticks = 0;
+            @Override
+            public void run() {
+                if (ticks >= 40 || !player.isOnline()) {
+                    cancel();
+                    return;
+                }
+                border.runBorderedOnSurface(1, 1, spawner);
+                ticks += 5;
+            }
+        }.runTaskTimer(TownyRoadsPlugin.getInstance(), 0L, 5L);
     }
 }
